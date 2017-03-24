ca0bfeac977f5e2e27b3bf9790f434d0838046d0
Ivan Pilyugin <jahglow@gmail.com>
2017-03-24 13:40:09 +0300

    envocation is transferred to Reportal

--- src/main.js
+++ src/main.js
@@ -3,10 +3,5 @@ import MDHierarchy from "./hierarchy/hierarchy";
 import TransformMenuAt from "./navigation/menu-transformer";
 
 window.Reportal = window.Reportal || {};
-window.Reportal.state = window.Reportal.state || {};
-
-window.addEventListener('DOMContentLoaded',()=>{
-  MDHierarchy.upgrade();
-
-  window.Reportal.state.TransformMenuAt = new TransformMenuAt(768)
-});
+window.Reportal.TransformMenuAt = TransformMenuAt;
+window.Reportal.hierarchyUpgrade = MDHierarchy.upgrade;
